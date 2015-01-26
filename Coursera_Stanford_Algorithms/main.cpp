83f627c7505adfbc2bf144e2e587cd8ada8fcce2
Vorren <pvmajev@gmail.com>
2015-01-26 22:52:16 +0300

    Initial Commit

---
+++ Coursera_Stanford_Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursera_Stanford_Algorithms
+//
+//  Created by Phil Maevskiy on 26/01/15.
+//  Copyright (c) 2015 Magic Forge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
